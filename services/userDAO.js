2d5de10d9923279b7982a289b0123f925c870a2e
Giovannix97 <g.iacovazzo2@studenti.unisa.it>
2021-01-11 10:00:21 +0100

    Implemented storage logic for user

--- services/userDAO.js
+++ services/userDAO.js
@@ -2,95 +2,130 @@
 
 const path = require('path');
 const dotenv = require('dotenv');
-const { Console } = require('console');
 
 // Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
 
-
-// Change this value with your partition
-const partitionKey = "ciao"
-
-
+/**
+ * Manages reading, adding, and updating user info in Cosmos DB
+ */
 class UserDAO {
   /**
-   * Manages reading, adding, and updating Tasks in Cosmos DB
+   * Construct a new userDAO. Should be initialized with init() function before use
    * @param {CosmosClient} cosmosClient
    * @param {string} databaseId
    * @param {string} containerId
    */
-  constructor(cosmosClient, databaseId, containerId) {
-    this.client = cosmosClient
-    this.databaseId = databaseId || process.env.DatabaseId;
-    this.collectionId = containerId || process.env.ContainerId;
+  constructor(cosmosClient, databaseId, containerId, partitionKey) {
+    /** @private */
+    this._cosmosClient = cosmosClient;
+    /** @private */
+    this._databaseId = databaseId || process.env.DatabaseId;
+    /** @private */
+    this._collectionId = containerId || process.env.ContainerId;
+    /** @private */
+    this._partitionKey = partitionKey || process.env.partitionKey;
 
-    if (!this.databaseId) throw ("You must specify a DatabaseID")
-    if (!this.collectionId) throw ("You must specify a CollectionID/ContainerID")
+    if (!this._databaseId) throw ("You must specify a DatabaseID")
+    if (!this._collectionId) throw ("You must specify a CollectionID/ContainerID")
+    if (!this._partitionKey) throw ("You must specify a Partition Key")
 
-    this.database = null
-    this.container = null
+    /** @private */
+    this._container = null
   }
 
-
+  /**
+   * Performs DAO initialization
+   */
   async init() {
     console.info('[INFO]: Setting up the database...');
-    const dbResponse = await this.client.databases.createIfNotExists({
-      id: this.databaseId
+    const dbResponse = await this._cosmosClient.databases.createIfNotExists({
+      id: this._databaseId
     })
-    this.database = dbResponse.database
+    const database = dbResponse.database;
     console.info('[INFO]: Setting up the database...done!');
     console.info('[INFO]: Setting up the container...');
-    const coResponse = await this.database.containers.createIfNotExists({
-      id: this.collectionId
-    })
-    this.container = coResponse.container
+    const containerResponse = await database.containers.createIfNotExists({ id: this._collectionId, partitionKey: { kind: "Hash", paths: [this._partitionKey] } });
+    this._container = containerResponse.container
     console.info('[INFO]: Setting up the container...done!');
   }
 
-
-  async find(querySpec) {
-    console.info('[INFO]: Querying for items from the database')
-    if (!this.container) {
-      throw new Error('Collection is not initialized.')
+    /**
+    * Find a user with the id and the channel
+    * @param {'telegram' | 'discord' | 'twitch' | 'web'} channel The channel user belongs to 
+    * @param {string} id The user identification for the channel
+    * @typedef {{id, channel}} User
+    * @returns {User} User ready to be stored
+    */
+   async find(query) {
+    if (!this._container) {
+      throw new Error(`Collection is not initilized for ${userDAO.name}. Please, be sure to call init() before calling this method`)
     }
-    const { resources } = await this.container.items.query(querySpec).fetchAll()
+    console.info('[INFO]: Querying for user from the database');
+    const { resources } = await this._container.items.query(query).fetchAll();
+    return resources
+  }
+  
+  /**
+    * Find a user with the id and the channel
+    * @param {'telegram' | 'discord' | 'twitch' | 'web'} channel The channel user belongs to 
+    * @param {string} id The user identification for the channel
+    * @typedef {{id, channel}} User
+    * @returns {User} User ready to be stored
+    */
+  async findById(channel, id) {
+    if (!this._container) {
+      throw new Error(`Collection is not initilized for ${userDAO.name}. Please, be sure to call init() before calling this method`)
+    }
+    console.info('[INFO]: Querying for user from the database');
+    const { resources } = await this._container.item.query(id, channel).read();
     return resources
   }
 
-
-  async addItem(item) {
-    console.info('[INFO]: Adding an item to the database...');
-    item.date = Date.now();
-    item.completed = false;
-    const { resource: doc } = await this.container.items.create(item);
-    console.info('[INFO]: Adding an item to the database... done!');
+  /**
+    * Store a new user on db
+    * @param {User} user User to be created
+    * @typedef {{id, channel}} User
+    */
+  async create(user) {
+    console.info('[INFO]: Adding a user to the database...');
+    const { resource: doc } = await this._container.items.create(user);
+    console.info('[INFO]: Adding a user to the database... done!');
     return doc;
   }
 
+  /**
+    * Update an user on the db with id equals to 
+    * @param {*} userID The id of the user
+    * @param {User} user The updated user object
+    * @typedef {{id, channel} User
+    */
+  async update(userId, user) {
+    console.info('[INFO]: Updating an user in the database...');
 
-  async updateItem(itemId) {
-    console.info('[INFO]: Updating an item in the database...');
-    const doc = await this.getItem(itemId);
-    // Property to modify
-    doc.completed = true;
+    const { resource: replaced } = await this._container
+      .item(userId, user[this._partitionKey])
+      .replace(user)
 
-    const { resource: replaced } = await this.container
-      .item(itemId, partitionKey)
-      .replace(doc)
-
-    console.info('[INFO]: Updating an item in the database... done!');
+    console.info('[INFO]: Updating an user in the database... done!');
     return replaced;
   }
 
+  /**
+    * Remove the specified user from db
+    * @param {User} user User to be removed
+    * @typedef {{id, channel}} User
+    */
+  async delete(user) {
+    console.info('[INFO]: Deleting an user from the database...');
 
-  async getItem(itemId) {
-    console.info('[INFO]: Getting an item from the database');
-    const { resource } = await this.container.item(itemId, partitionKey).read();
-    console.log('[INFO]: Getting an item from the database... done!');
-    return resource;
+    await this._container
+      .item(user.id, user[this._partitionKey])
+      .delete();
+
+    console.info('[INFO]: Deleting an user from the database... done!');
   }
 }
-
 
 module.exports.userDAO = UserDAO;
